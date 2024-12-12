7c6072b7e8e15431523945a66df4dcb9c8ffd616
William T <33132686+Williamt999@users.noreply.github.com>
2024-12-12 18:07:16 +0800

    Update SkyAI.py

--- application/SkyAI.py
+++ application/SkyAI.py
@@ -20,26 +20,17 @@ import streamlit as st
 
 #st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-import base64
-
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
+st.markdown(
+    """
     <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
+    .reportview-container {
+        background: url('https://github.com/Williamt999/skyai-app/blob/main/data/sky-ai-background-home-6.png')
+    }
+   .sidebar .sidebar-content {
+        background: url('https://github.com/Williamt999/skyai-app/blob/main/data/sky-ai-background-home-6.png')
     }
     </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
+    """,
+    unsafe_allow_html=True
+)
 
-set_png_as_page_bg('./data/sky-ai-background-home-6.png')
