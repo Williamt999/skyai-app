9575d07bd75060e7f8dafcdd4860978cefa28194
William T <33132686+Williamt999@users.noreply.github.com>
2024-12-12 17:48:57 +0800

    Update SkyAI.py

--- application/SkyAI.py
+++ application/SkyAI.py
@@ -2,18 +2,20 @@
 import streamlit as st
 
 # CSS styyling to add home page background image & hide navbar color
-css = '''
-.stApp {
+#st.image('https://github.com/Williamt999/skyai-app/blob/main/data/sky-ai-background-home-6.png')
 
-    background-image: url('https://github.com/Williamt999/skyai-app/blob/main/data/sky-ai-background-home-6.png');
-    background-size: cover;
+#css = '''
+#.stApp {
 
-}
+#    background-image: url('https://github.com/Williamt999/skyai-app/blob/main/data/sky-ai-background-home-6.png');
+#    background-size: cover;
 
-.stApp > header {
-    background-color: transparent;
-}
+#}
 
-'''
+#.stApp > header {
+#    background-color: transparent;
+#}
 
-st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+#'''
+
+#st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
