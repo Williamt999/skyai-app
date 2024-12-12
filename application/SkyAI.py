c4165b30ddf55b56debf821af05ae742f3f84aa7
William T <33132686+Williamt999@users.noreply.github.com>
2024-12-12 18:02:22 +0800

    Update SkyAI.py

--- application/SkyAI.py
+++ application/SkyAI.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 # CSS styyling to add home page background image & hide navbar color
-st.image('./data/sky-ai-background-home-6.png',use_container_width=True)
+# st.image('./data/sky-ai-background-home-6.png',use_container_width=True)
 
 #css = '''
 #.stApp {
@@ -19,3 +19,27 @@ st.image('./data/sky-ai-background-home-6.png',use_container_width=True)
 #'''
 
 #st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('./data/sky-ai-background-home-6.png')
